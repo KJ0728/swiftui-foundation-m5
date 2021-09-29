bf3dd05185ece035143cb34afe9d91ad5334dbe4
Kun <lee.kunjong@gmail.com>
2021-09-29 17:11:09 +0900

    Initial Commit

---
+++ LearningApp/LearningAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  LearningAppApp.swift
+//  LearningApp
+//
+//  Created by Kun Jong Lee on 2021/09/29.
+//
+
+import SwiftUI
+
+@main
+struct LearningAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
