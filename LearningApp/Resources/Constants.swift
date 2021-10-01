74b5f2643005916591104ac5501b2f0e18343562
Kun <lee.kunjong@gmail.com>
2021-10-01 10:54:47 +0900

    Add ContentDetail View Add Constant File

---
+++ LearningApp/Resources/Constants.swift
@@ -0,0 +1,14 @@
+//
+//  Constants.swift
+//  LearningApp
+//
+//  Created by Kun Jong Lee on 2021/10/01.
+//
+
+import Foundation
+
+struct Constants {
+    
+    static var videoHostUrl = "https://kj0728.github.io/swiftui-foundation-m5/"
+    
+}
---
+++ LearningApp/Views/ContentDetailView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentDetailView.swift
+//  LearningApp
+//
+//  Created by Kun Jong Lee on 2021/10/01.
+//
+
+import SwiftUI
+
+struct ContentDetailView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct ContentDetailView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentDetailView()
+    }
+}
